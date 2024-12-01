8795187b2c329443d512357d2ff679605e7d3885
TangWan <tangjingfan@gmail.com>
2024-12-01 17:11:36 +0800

    feat: init control code

---
+++ src/main/main.ino
@@ -0,0 +1,12 @@
+#include "mecanum.h"
+#include "mecanum_driver.h"
+#include "obstacle_avoidance.h"
+#include "rplidar_driver.h"
+
+void setup() {
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
