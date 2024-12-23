dda8b5c679139e36c9eccf420bffa90baef28435
tang_jingfan <tangjingfan@gmail.com>
2024-12-23 15:30:56 +0800

    feat: add obstacle_avoidance

--- workspace/src/car_moving/src/move/move.ino
+++ workspace/src/car_moving/src/move/move.ino
@@ -1,4 +1,7 @@
-#include ""
+#include "workspace/src/car_moving/sdk/include/MecanumDriver.h"
+#include "workspace/src/car_moving/sdk/include/MotorDriver.h"
+
+MecanumDriver mecanum(9, 8, 12, 13, 11, 10, 46, 21);
 
 void setup() {}
 
