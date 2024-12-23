93c69a349d66818afd728599d8e697a7bc12a547
tang_jingfan <tangjingfan@gmail.com>
2024-12-23 10:36:43 +0800

    feat: add rplidar_ros

---
+++ workspace/build/rplidar_ros/CMakeFiles/rplidarNodeClient.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/rplidarNodeClient.dir/src/client.cpp.o"
+  "/home/nano/Autonomous-Ball-Pick-up-Car/workspace/devel/lib/rplidar_ros/rplidarNodeClient.pdb"
+  "/home/nano/Autonomous-Ball-Pick-up-Car/workspace/devel/lib/rplidar_ros/rplidarNodeClient"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/rplidarNodeClient.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
