aaf3bddc546cbf480f63b9e2821b794080984d62
tang_jingfan <tangjingfan@gmail.com>
2024-12-26 20:39:44 +0800

    fix: low baudrate

--- workspace/src/laser_to_arduino/src/laser_to_arduino.cpp
+++ workspace/src/laser_to_arduino/src/laser_to_arduino.cpp
@@ -37,7 +37,7 @@ int main(int argc, char **argv) {
   // init serial
   try {
     ser.setPort("/dev/ttyUSB1"); // modify it to real serial
-    ser.setBaudrate(9600);
+    ser.setBaudrate(115200);
     serial::Timeout to = serial::Timeout::simpleTimeout(1000);
     ser.setTimeout(to);
     ser.open();
