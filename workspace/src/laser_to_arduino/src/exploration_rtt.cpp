cdb62abf6371abaa24adfed9a8992c1d96fa5808
tang_jingfan <tangjingfan@gmail.com>
2025-01-01 23:16:01 +0800

    feat: init rrt exploration

---
+++ workspace/src/laser_to_arduino/src/exploration_rtt.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <vector>
+
+// -1: unknown area
+// 0: free area
+// 1: obstacle
+
+const int map_size_x = 100;
+const int map_size_y = 100;
+// resolution 0.1 meter in real world
+const double resolution = 0.1;
