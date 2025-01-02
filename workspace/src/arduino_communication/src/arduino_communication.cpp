c010fc6ebeb6c6e552a9679dcf91fe393ddb7b02
tang_jingfan <tangjingfan@gmail.com>
2025-01-02 16:50:02 +0800

    feat: add ros info

--- workspace/src/arduino_communication/src/arduino_communication.cpp
+++ workspace/src/arduino_communication/src/arduino_communication.cpp
@@ -15,9 +15,10 @@ void laser_call_back(const sensor_msgs::LaserScan::ConstPtr &msg) {
     ser.write("<100,100,100,100>");
     // flush output buffer
     ser.flushOutput();
-    std::string received_msgs = ser.read();
+    ROS_INFO("Sent!");
+    // std::string received_msgs = ser.read();
     // ROS_INFO("Sent to Arduino: %s", received_msgs.c_str());
-    std::cout << received_msgs << std::endl;
+    // std::cout << received_msgs << std::endl;
     // ros::Duration(0.1).sleep(); // Sleep for 100 milliseconds
   } catch (serial::IOException &e) {
     ROS_ERROR("Unable to send data to Arduino.");
