6b136fbd5365180d71d6a0c1bb356239aade125c
tang_jingfan <tangjingfan@gmail.com>
2025-01-05 22:17:13 +0800

    fix: wrong coefficient

--- workspace/src/explore/src/explore_control.cpp
+++ workspace/src/explore/src/explore_control.cpp
@@ -22,11 +22,11 @@ get_next_step_velocity(const sensor_msgs::LaserScan::ConstPtr &msg) {
     float distance = msg->ranges[i];
     float angle = msg->angle_min + i * msg->angle_increment;
 
-    if (angle < 1.57 && angle > 0.52 && distance * cos(angle) < 0.2 &&
+    if (angle < 1.57 && angle > 0.52 && distance * sin(angle) < 0.2 &&
         distance >= msg->range_min) {
       left_empty = false;
     }
-    if (angle < 0.52 && angle > -0.52 && distance * sin(angle) < 0.25 &&
+    if (angle < 0.52 && angle > -0.52 && distance * cos(angle) < 0.25 &&
         distance >= msg->range_min) {
       forward_empty = false;
     }
