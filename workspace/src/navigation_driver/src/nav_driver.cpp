f63653645728d0ad07e26b559308cbf327af671a
tang_jingfan <tangjingfan@gmail.com>
2025-01-14 00:36:34 +0800

    feat: add cmd_vel callback function

--- workspace/src/navigation_driver/src/nav_driver.cpp
+++ workspace/src/navigation_driver/src/nav_driver.cpp
@@ -1,14 +1,36 @@
-#include <cmath>
-#include <iostream>
+#include <geometry_msgs/Twist.h>
 #include <ros/ros.h>
-#include <sensor_msgs/LaserScan.h>
 #include <serial/serial.h>
-#include <sstream>
-#include <vector>
+
+serial::Serial ser;
+
+void cmd_vel_call_back(const geometry_msgs::Twist::ConstPtr &msg) {}
 
 int main(int argc, char **argv) {
   ros::init(argc, argc, "nav_driver");
   ros::NodeHandle nh;
 
-  ros::Subscriber sub = nh.subscribe("/cmd_vel", 1, cmd_vel_call_back);
+  try {
+    ser.setPort("/dev/arduino");
+    ser.setBaudrate(115200);
+    serial::Timeout to = serial::Timeout::simpleTimeout(1000);
+    ser.setTimeout(to);
+    ser.open();
+  } catch (serial::IOException &e) {
+    ROS_ERROR("Unable to open port.");
+    return -1;
+  }
+
+  if (ser.isOpen()) {
+    ROS_INFO("Serial Port initialized.");
+  } else {
+    return -1;
+  }
+
+  ros::Subscriber sub = nh.subscribe("/cmd_vel", 10, cmd_vel_call_back);
+
+  ros::spin();
+
+  ser.close();
+  return 0;
 }
