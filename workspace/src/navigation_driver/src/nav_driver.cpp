44f1a960238b50fcb109525ecf8ece7116c60979
tang_jingfan <tangjingfan@gmail.com>
2025-01-15 11:25:25 +0800

    feat: nesting function declaration

--- workspace/src/navigation_driver/src/nav_driver.cpp
+++ workspace/src/navigation_driver/src/nav_driver.cpp
@@ -6,35 +6,36 @@
 
 serial::Serial ser;
 
-// send info to arduino
-void cmd_vel_call_back(const geometry_msgs::Twist::ConstPtr &msg) {
-  // linear velocity
-  double linear_x = msg->linear.x;
-  // angular velocity
-  double angular_z = msg->angular.z;
+std::string generate_control_command(double linear_x, double angular_z) {
+  std::string command = "<0,0,0,0>"; // init command
 
-  std::string generate_control_command(double linear_x, double angular_z) {
-    std::string command = "<0,0,0,0>"; // init command
+  // set command for chassis
+  if (linear_x > 0) {
+    // forward
+    command =
+        "<" + std::to_string(static_cast<int>(linear_x * 100)) + ",0,0,0>";
+  } else if (linear_x < 0) {
+    // backward
+    command =
+        "<0,0," + std::to_string(static_cast<int>(-linear_x * 100)) + ",0>";
+  }
 
-    // set command for chassis
-    if (linear_x > 0) {
-      // forward
-      command =
-          "<" + std::to_string(static_cast<int>(linear_x * 100)) + ",0,0,0>";
-    } else if (linear_x < 0) {
-      // backward
-      command =
-          "<0,0," + std::to_string(static_cast<int>(-linear_x * 100)) + ",0>";
-    }
+  if (angular_z > 0) {
+    command = "<" + std::to_string(static_cast<int>(linear_x * 100)) + ",0," +
+              std::to_string(static_cast<int>(angular_z * 100)) + ",0>";
+  } else if (angular_z < 0) {
+    command = "<" + std::to_string(static_cast<int>(linear_x * 100)) + ",0,0," +
+              std::to_string(static_cast<int>(-angular_z * 100)) + ">";
+  }
 
-    if (angular_z > 0) {
-      command = "<" + std::to_string(static_cast<int>(linear_x * 100)) + ",0," +
-                std::to_string(static_cast<int>(angular_z * 100)) + ",0>";
-    } else if (angular_z < 0) {
-      command = "<" + std::to_string(static_cast<int>(linear_x * 100)) +
-                ",0,0," + std::to_string(static_cast<int>(-angular_z * 100)) +
-                ">";
-    }
+  // send info to arduino
+  void cmd_vel_call_back(const geometry_msgs::Twist::ConstPtr &msg) {
+    // linear velocity
+    double linear_x = msg->linear.x;
+    // angular velocity
+    double angular_z = msg->angular.z;
+
+    std::string = generate_control_command(linear_x, angular_z);
 
     try {
       ser.write(command);
