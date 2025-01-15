fbc9266ec224f44e931919a941f697ec9b2f95d4
tang_jingfan <tangjingfan@gmail.com>
2025-01-15 19:39:21 +0800

    fix: too small action time

--- workspace/src/active_move/active_move.ino
+++ workspace/src/active_move/active_move.ino
@@ -3,7 +3,7 @@
 // define pin for mecanum car
 MecanumDriver mecanum(9, 8, 12, 13, 11, 10, 46, 21);
 
-//define an array to record speed for 4 wheels
+// define an array to record speed for 4 wheels
 int speed[4] = {0, 0, 0, 0};
 
 // define a string to receive command from serial
@@ -42,5 +42,5 @@ void loop() {
 
   // make mecanum car run
   mecanum.setDutyCycle(speed[0], speed[1], speed[2], speed[3]);
-  delay(20);
+  delay(500);
 }
