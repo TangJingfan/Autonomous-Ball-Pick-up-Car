cc2349ae91f037a10fb84a7d5cffde028997aaed
tang_jingfan <tangjingfan@gmail.com>
2025-01-15 16:04:13 +0800

    feat: update param

--- workspace/src/navigation_driver/src/nav_driver.cpp
+++ workspace/src/navigation_driver/src/nav_driver.cpp
@@ -22,9 +22,11 @@ std::string generate_control_command(double linear, double angular) {
 
   // step 3. calculate corresponding voltage
   int voltage_left = floor(
-      (2 / 3) * (linear / 0.4 * 255 - (angular * width) / (2 * 0.3) * 255));
+      /*(2 / 3) * */ (linear / 0.4 * 255 -
+                      (angular * width) / (2 * 0.3) * 255));
   int voltage_right = floor(
-      (2 / 3) * (linear / 0.4 * 255 + (angular * width) / (2 * 0.3) * 255));
+      /*(2 / 3) * */ (linear / 0.4 * 255 +
+                      (angular * width) / (2 * 0.3) * 255));
 
   // step 4. set command
   command = "<" + std::to_string(voltage_left) + "," +
