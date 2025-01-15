9faefd2a680fe0fe938f12b69847fcb275c1d054
tang_jingfan <tangjingfan@gmail.com>
2025-01-15 19:07:33 +0800

    fix: voltage calculation

--- workspace/src/navigation_driver/src/nav_driver.cpp
+++ workspace/src/navigation_driver/src/nav_driver.cpp
@@ -22,10 +22,10 @@ std::string generate_control_command(double linear, double angular) {
 
   // step 3. calculate corresponding voltage
   int voltage_left = floor(
-      /*(2 / 3) * */ (linear / 0.4 * 255 -
+      /*(2 / 3) * */ (linear / 0.4 * 255 +
                       (angular * width) / (2 * 0.3) * 255));
   int voltage_right = floor(
-      /*(2 / 3) * */ (linear / 0.4 * 255 +
+      /*(2 / 3) * */ (linear / 0.4 * 255 -
                       (angular * width) / (2 * 0.3) * 255));
 
   // step 4. set command
