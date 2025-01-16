3b5d9df436d866356111601892ed0871dafca70b
tang_jingfan <tangjingfan@gmail.com>
2025-01-16 22:20:56 +0800

    feat: add cv related, draw route

--- workspace/src/abp_local_planner/include/abp_local_planner.h
+++ workspace/src/abp_local_planner/include/abp_local_planner.h
@@ -7,10 +7,15 @@
 namespace abp_local_planner {
 class ABPLocalPlanner : public nav_core::BaseLocalPlanner {
 public:
+  // init at a very early time
+  // cannot subscribe a topic or publish one
   ABPLocalPlanner();
   ~ABPLocalPlanner();
+  // major control
   bool computeVelocityCommands(geometry_msgs::Twist &cmd_vel);
+  // set global plan
   bool setPlan(const std::vector<geometry_msgs::PoseStamped> &plan);
+  // here subscribe a topic or publish one
   void initialize(std::string name, tf2_ros::Buffer *tf,
                   costmap_2d::Costmap2DROS *costmap_ros);
   bool isGoalReached();
--- workspace/src/abp_local_planner/src/abp_local_planner.cpp
+++ workspace/src/abp_local_planner/src/abp_local_planner.cpp
@@ -1,5 +1,8 @@
 #include "abp_local_planner.h"
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
 #include <pluginlib/class_list_macros.h>
+#include <vector>
 
 PLUGINLIB_EXPORT_CLASS(abp_local_planner::ABPLocalPlanner,
                        nav_core::BaseLocalPlanner)
@@ -10,16 +13,28 @@ ABPLocalPlanner::ABPLocalPlanner() {}
 ABPLocalPlanner::~ABPLocalPlanner() {}
 
 void ABPLocalPlanner::initialize(std::string name, tf2_ros::Buffer *tf,
-                                 costmap_2d::Costmap2DROS *costmap_ros) {
-  ROS_INFO("Use customized local planner!")
-}
+                                 costmap_2d::Costmap2DROS *costmap_ros){
+    ROS_INFO("Use customized local planner!")}
+
+std::vector<geometry_msgs::PoseStamped> global_plan_;
 
 bool ABPLocalPlanner::setPlan(
     const std::vector<geometry_msgs::PoseStamped> &plan) {
+  global_plan_ = plan;
   return true;
 }
 
 bool ABPLocalPlanner::computeVelocityCommands(geometry_msgs::Twist &cmd_vel) {
+  cv::Mat plan_image(1000, 1000, CV_8UC3, cv::Scalar(0, 0, 0));
+  for (int i = 0; i < global_plan_.size(); i++) {
+    int cv_x = 500 - gloal_plan[i].pose.position.y * 100;
+    int cv_y = 500 - gloal_plan[i].pose.position.x * 100;
+    cv::circle(plan_image, cv::Point(cv_x, cv_y), 1, cv::Scalar(255, 0, 255));
+  }
+
+  cv::nameWindow("Plan");
+  cv::imshow("Plan", plan_image);
+  cv::waitKey(1);
   return true;
 }
 
