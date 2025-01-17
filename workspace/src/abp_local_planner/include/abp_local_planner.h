3c68023819794846a0c25d9db5dae5d372db9bea
tang_jingfan <tangjingfan@gmail.com>
2025-01-17 19:59:04 +0800

    feat: add local planner

---
+++ workspace/src/abp_local_planner/include/abp_local_planner.h
@@ -0,0 +1,26 @@
+#ifndef ABP_LOCAL_PLANNER_H
+#define ABP_LOCAL_PLANNER_H
+
+#include <nav_core/base_local_planner.h>
+#include <ros/ros.h>
+
+namespace abp_local_planner {
+class ABPLocalPlanner : public nav_core::BaseLocalPlanner {
+public:
+  // init at a very early time
+  // cannot subscribe a topic or publish one
+  ABPLocalPlanner();
+  ~ABPLocalPlanner();
+  // major control
+  bool computeVelocityCommands(geometry_msgs::Twist &cmd_vel);
+  // set global plan
+  bool setPlan(const std::vector<geometry_msgs::PoseStamped> &plan);
+  // here subscribe a topic or publish one
+  void initialize(std::string name, tf2_ros::Buffer *tf,
+                  costmap_2d::Costmap2DROS *costmap_ros);
+  bool isGoalReached();
+};
+
+} // namespace abp_local_planner
+
+#endif
---
+++ workspace/src/abp_local_planner/src/abp_local_planner.cpp
@@ -0,0 +1,43 @@
+#include "abp_local_planner.h"
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <pluginlib/class_list_macros.h>
+#include <vector>
+
+PLUGINLIB_EXPORT_CLASS(abp_local_planner::ABPLocalPlanner,
+                       nav_core::BaseLocalPlanner)
+
+namespace abp_local_planner {
+ABPLocalPlanner::ABPLocalPlanner() {}
+
+ABPLocalPlanner::~ABPLocalPlanner() {}
+
+void ABPLocalPlanner::initialize(std::string name, tf2_ros::Buffer *tf,
+                                 costmap_2d::Costmap2DROS *costmap_ros) {
+  ROS_INFO("Use customized local planner!");
+}
+
+std::vector<geometry_msgs::PoseStamped> global_plan_;
+
+bool ABPLocalPlanner::setPlan(
+    const std::vector<geometry_msgs::PoseStamped> &plan) {
+  global_plan_ = plan;
+  return true;
+}
+
+bool ABPLocalPlanner::computeVelocityCommands(geometry_msgs::Twist &cmd_vel) {
+  cv::Mat plan_image(1000, 1000, CV_8UC3, cv::Scalar(0, 0, 0));
+  for (int i = 0; i < global_plan_.size(); i++) {
+    int cv_x = 500 - global_plan_[i].pose.position.y * 100;
+    int cv_y = 500 - global_plan_[i].pose.position.x * 100;
+    cv::circle(plan_image, cv::Point(cv_x, cv_y), 1, cv::Scalar(255, 0, 255));
+  }
+
+  cv::namedWindow("Plan");
+  cv::imshow("Plan", plan_image);
+  cv::waitKey(1);
+  return true;
+}
+
+bool ABPLocalPlanner::isGoalReached() { return false; }
+} // namespace abp_local_planner
