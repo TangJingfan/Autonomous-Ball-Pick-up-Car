590f4cc58e256aa451733872b7a495a9e5a13697
tang_jingfan <tangjingfan@gmail.com>
2025-01-25 22:18:10 +0800

    feat: a node to print image

--- workspace/src/ball_detection/src/ball_detection_node.cpp
+++ workspace/src/ball_detection/src/ball_detection_node.cpp
@@ -6,10 +6,25 @@
 
 using namespace cv;
 
-void img_callback() {}
+void img_callback(const sensor_msgs::Image msg) {
+  cv_bridge::CvImagePtr cv_ptr;
+  try {
+    cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
+  } catch (cv_bridge::exception &e) {
+    ROS_ERROR("cv bridge exception: %s", e.what());
+    return;
+  }
+  Mat img_original = cv_ptr->image;
+  imshow("RGB", img_original);
+  waitkey(1);
+}
 
 int main(int argc, char **argv) {
   ros::init(argc, argv, "ball_detection");
   ros::NodeHandle nh;
-  ros::Subscriber img_sub = nh.Subscribe("/usb_cam/image_raw", 1, img_callback);
+  ros::Subscriber img_sub =
+      nh.Subscribe("/usb_cam/image_rect_color", 1, img_callback);
+
+  namedWindow("RGB");
+  ros::spin();
 }
