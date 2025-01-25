7cda46de79f0c01871daec0b7db5fc96bfce6b21
tang_jingfan <tangjingfan@gmail.com>
2025-01-25 17:30:35 +0800

    feat: update cali

--- workspace/src/camera_view/script/cam_calibration.sh
+++ workspace/src/camera_view/script/cam_calibration.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-rosrun camera_calibration cameracalibrator.py --size 8x6 --square 0.025 image:=/usb_cam/image_raw
+rosrun camera_calibration cameracalibrator.py --size 8x6 --square 0.1 image:=/usb_cam/image_raw camera:=/head_camera --no-service-check
